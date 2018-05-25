c99ef52a0f0d069ebd5aeb5ede373f8144edc982
Michal Charemza <michal@charemza.name>
2018-05-25 13:03:31 +0100

    (feat) Add a timeout to catch more JS

--- content.js
+++ content.js
@@ -1,9 +1,12 @@
 
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     if (msg.text === 'get_links') {
-        var links = Array.from(document.body.getElementsByTagName("a")).map(function(a) {
-            return a.href;
-        });
-        sendResponse({links: links});
+        setTimeout(function() {
+            var links = Array.from(document.body.getElementsByTagName("a")).map(function(a) {
+                return a.href;
+            });
+            sendResponse({links: links});
+        }, 1000);
+        return true;
     }
 });
