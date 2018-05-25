ff86da0b5df94d6279fea39a1cae85dc40fa0ead
Michal Charemza <michal@charemza.name>
2018-05-25 11:34:56 +0100

    (refactor) Make settings available to more code

--- background.js
+++ background.js
@@ -5,20 +5,6 @@ var startingHost = "";
 var startingPage = {};
 var appState = "stopped";
 
-var settings = 
-{
-    get maxDepth() { return localStorage["max-crawl-depth"]!=null?localStorage["max-crawl-depth"]:2; },
-    set maxDepth(val) { localStorage['max-crawl-depth'] = val; },
-    
-    get interestingFileTypes() 
-    { 
-        var types = (localStorage["interesting-file-types"]!=null?localStorage["interesting-file-types"]:"flv,mk4,ogg,swf,avi,mp3,zip,png,gif,jpg").split(",");
-        for (var i in types) { types[i] = $.trim(types[i]);  }
-        return types;
-    },
-    set interestingFileTypes(val) { localStorage['interesting-file-types'] = val; }
-}
-
 function beginCrawl(url)
 {   
     reset();    
