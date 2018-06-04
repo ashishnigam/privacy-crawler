930290920d536a9bdc9d8af1f52aadbdee8857a8
Michal Charemza <michal@charemza.name>
2018-06-04 14:57:49 +0100

    (refactor) Remove unnecessary logging

--- instrument.js
+++ instrument.js
@@ -79,88 +79,6 @@ function instrument() {
 
     var testing = false;
 
-    // Recursively generates a path for an element
-    function getPathToDomElement(element, visibilityAttr=false) {
-      if(element == document.body)
-        return element.tagName;
-      if(element.parentNode == null)
-        return 'NULL/' + element.tagName;
-
-      var siblingIndex = 1;
-      var siblings = element.parentNode.childNodes;
-      for (var i = 0; i < siblings.length; i++) {
-        var sibling = siblings[i];
-        if (sibling == element) {
-          var path = getPathToDomElement(element.parentNode, visibilityAttr);
-          path += '/' + element.tagName + '[' + siblingIndex;
-          path += ',' + element.id;
-          path += ',' + element.className;
-          if (visibilityAttr) {
-            path += ',' + element.hidden;
-            path += ',' + element.style.display;
-            path += ',' + element.style.visibility;
-          }
-          if(element.tagName == 'A')
-            path += ',' + element.href;
-          path += ']';
-          return path;
-        }
-        if (sibling.nodeType == 1 && sibling.tagName == element.tagName)
-          siblingIndex++;
-      }
-    }
-
-    // Helper for JSONifying objects
-    function serializeObject(object, stringifyFunctions=false) {
-
-      // Handle permissions errors
-      try {
-        if(object == null)
-          return "null";
-        if(typeof object == "function") {
-          if (stringifyFunctions)
-            return object.toString();
-          else
-            return "FUNCTION";
-        }
-        if(typeof object != "object")
-          return object;
-        var seenObjects = [];
-        return JSON.stringify(object, function(key, value) {
-          if(value == null)
-            return "null";
-          if(typeof value == "function") {
-            if (stringifyFunctions)
-              return value.toString();
-            else
-              return "FUNCTION";
-          }
-          if(typeof value == "object") {
-            // Remove wrapping on content objects
-            if("wrappedJSObject" in value) {
-              value = value.wrappedJSObject;
-            }
-
-            // Serialize DOM elements
-            if(value instanceof HTMLElement)
-              return getPathToDomElement(value);
-
-            // Prevent serialization cycles
-            if(key == "" || seenObjects.indexOf(value) < 0) {
-              seenObjects.push(value);
-              return value;
-            }
-            else
-              return typeof value;
-          }
-          return value;
-        });
-      } catch(error) {
-        console.log("SERIALIZATION ERROR: " + error);
-        return "SERIALIZATION ERROR: " + error;
-      }
-    }
-
     function logErrorToConsole(error) {
       console.log("Error name: " + error.name);
       console.log("Error message: " + error.message);
@@ -288,7 +206,6 @@ function instrument() {
       var msg = {
         operation: operation,
         name: instrumentedVariableName,
-        value: serializeObject(value, !!logSettings.logFunctionsAsStrings),
         scriptUrl: callContext.scriptUrl,
         scriptLine: callContext.scriptLine,
         scriptCol: callContext.scriptCol,
@@ -322,15 +239,9 @@ function instrument() {
       }
 
       try {
-        // Convert special arguments array to a standard array for JSONifying
-        var serialArgs = [ ];
-        for(var i = 0; i < args.length; i++)
-          serialArgs.push(serializeObject(args[i], !!logSettings.logFunctionsAsStrings));
         var msg = {
           operation: "call",
           name: instrumentedFunctionName,
-          args: serialArgs,
-          value: "",
           scriptUrl: callContext.scriptUrl,
           scriptLine: callContext.scriptLine,
           scriptCol: callContext.scriptCol,
