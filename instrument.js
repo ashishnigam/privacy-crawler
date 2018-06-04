1c257d5c328b352fa9ee9b920785d13393d6d246
Michal Charemza <michal@charemza.name>
2018-06-04 15:55:18 +0100

    (feat) Use throttle rather than debounce
    If the functions keep on being called, we don't want to miss messages

--- instrument.js
+++ instrument.js
@@ -16,44 +16,42 @@ function instrument() {
   // Intrumentation injection code is based on OpenWPM, in-tern based on privacybadgerfirefox
   // https://github.com/EFForg/privacybadgerfirefox/blob/master/data/fingerprinting.js
 
-  // from Underscore v1.6.0
-  function debounce(func, wait, immediate) {
-    var timeout, args, context, timestamp, result;
-
-    var later = function () {
-      var last = Date.now() - timestamp;
-      if (last < wait) {
-        timeout = setTimeout(later, wait - last);
-      } else {
-        timeout = null;
-        if (!immediate) {
-          result = func.apply(context, args);
-          context = args = null;
-        }
-      }
+  // https://stackoverflow.com/a/27078401
+  function throttle(func, wait, options) {
+    var context, args, result;
+    var timeout = null;
+    var previous = 0;
+    if (!options) options = {};
+    var later = function() {
+      previous = options.leading === false ? 0 : Date.now();
+      timeout = null;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
     };
-
-    return function () {
+    return function() {
+      var now = Date.now();
+      if (!previous && options.leading === false) previous = now;
+      var remaining = wait - (now - previous);
       context = this;
       args = arguments;
-      timestamp = Date.now();
-      var callNow = immediate && !timeout;
-      if (!timeout) {
-        timeout = setTimeout(later, wait);
-      }
-      if (callNow) {
+      if (remaining <= 0 || remaining > wait) {
+        if (timeout) {
+          clearTimeout(timeout);
+          timeout = null;
+        }
+        previous = now;
         result = func.apply(context, args);
-        context = args = null;
+        if (!timeout) context = args = null;
+      } else if (!timeout && options.trailing !== false) {
+        timeout = setTimeout(later, remaining);
       }
-
       return result;
     };
   }
-  // End of Debounce
 
   var send = (function () {
     var messages = [];
-    var _send = debounce(function () {
+    var _send = throttle(function () {
       document.dispatchEvent(new CustomEvent(event_id, {
         detail: messages
       }));
